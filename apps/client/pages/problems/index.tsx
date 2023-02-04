674dbfbaf280f63df29fc0ac0354651dd9ebb349
Yasin Torun <yasintorun4680@gmail.com>
2023-02-04 17:55:03 +0300

    update problems page

--- apps/client/pages/problems/index.tsx
+++ apps/client/pages/problems/index.tsx
@@ -1,5 +1,6 @@
 import KcLayout from '@/layouts/KcLayout'
 import Head from 'next/head'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
 
@@ -14,68 +15,63 @@ const TrackSlug = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div>
+            <div className='lg:w-8/9 md:w-11/12 m-auto'>
                 <section className="py-20 tails-selected-element">
-                    <div className="px-4 max-w-7xl sm:px-6 lg:px-8">
-                        <div className='flex items-center space-x-6'>
-                            <div className="avatar">
-                                <div className="w-20 rounded-full">
-                                    <img src="https://res.cloudinary.com/kodchallenge/image/upload/v1652446325/logos/cpp.png" />
-                                </div>
-                            </div>
-                            <h2 className="text-3xl font-extrabold tracking-tight text-gray-100 sm:text-4xl md:text-5xl xl:text-6xl">
-                                C++
-                            </h2>
-                        </div>
-                        <p className="mt-3 text-base text-gray-500 md:mt-5">
-                            C++ (Türkçe telaffuz: si plas plas), 1979'da Bell Laboratuarlarında başlayarak Bjarne Stroustrup tarafından geliştirilen genel amaçlı bir programlama dilidir. Özellikle hız veya bazı düşük seviye özelliklere erişim gerektiren uygulamalar için son derece popülerdir. Hem yüksek hem de düşük seviyeli dil özelliklerini kapsadığı için orta seviye bir dil olarak kabul edilir.
-                            C++ prosedürel, nesne yönelimli(Object Oriented), işlevsel(Functional) ve genel programlamayı destekler. C++ için derleyiciler, Windows, Mac OS ve Linux dahil olmak üzere esasen her platform için mevcuttur.
-                            <a href='https://tr.wikipedia.org/wiki/C%2B%2B' className='link px-3'>Wikipedia</a>
-                        </p>
-                    </div>
-                    <div className="tails-hover-element pt-12">
-                        <h3 className='font-bold tracking-tight py-6'>Tüm Problemler</h3>
-                        <div className='lg:grid grid-cols-12 gap-4 space-y-6 md:space-y-0 pt-8 sm:pt-0'>
-                            <div className='col-span-8 space-y-4'>
-                                {Array.from({ length: 3 }).map(problem => (
-                                    <div className="card w-full bg-base-100 shadow-xl hover:-translate-y-2 ease duration-100">
-                                        <div className="card-body space-y-3 sm:space-y-0 p-2 sm:p-8 sm:flex sm:items-center sm:flex-row justify-between">
-                                            <div>
-                                                <h2 className="card-title">Ardışık Sayıların Toplamı</h2>
-                                                <div className='space-x-2'>
-                                                    <div className="badge badge-success">Kolay</div>
-                                                    <div className="badge badge-primary">Skor: <strong>10</strong></div>
-                                                </div>
-                                            </div>
-                                            <div className="card-actions">
-                                                <button className="btn btn-primary">Problemi Çöz</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))}
+                    <div className="tails-hover-element">
+                        <h1 className='font-bold tracking-tight my-6'>Tüm Problemler</h1>
+                        <div className='lg:grid grid-cols-12 gap-4 lg:gap-10 space-y-6 md:space-y-0 pt-8 sm:pt-0'>
+                            <div className="overflow-x-auto col-span-8 space-y-4">
+                                <table className="table  table-zebra w-full">
+                                    <thead>
+                                        <tr>
+                                            <th>Durum</th>
+                                            <th>Başlık</th>
+                                            <th>Başarı Oranı</th>
+                                            <th>Skor</th>
+                                            <th>Zorluk</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {Array.from({ length: 13 }).map((problem, i) => (
+                                            <tr>
+                                                <td>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor" className="mr-1 h-[18px] w-[18px] text-yellow dark:text-dark-yellow"><path fill-rule="evenodd" d="M8.972 3a1 1 0 01.964.649l4.978 13.274 1.632-5.221A1 1 0 0117.5 11H21a1 1 0 110 2h-2.765l-2.28 7.298a1 1 0 01-1.891.053L9.086 7.077l-1.632 5.221A1 1 0 016.5 13H3a1 1 0 110-2h2.765l2.28-7.298A1 1 0 018.973 3z" clip-rule="evenodd"></path></svg>
+                                                </td>
+                                                <td>
+                                                    <Link href={"#"} className="">{i + 1}. Ardışık Sayıların Toplamı</Link>
+                                                </td>
+                                                <td>%87</td>
+                                                <td>20</td>
+                                                <td>Kolay</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
                             </div>
                             <div className='col-span-4'>
-                                <div className="card card-bordered w-full bg-transparent shadow-xl">
-                                    <div className="card-body space-y-3 sm:space-y-0 p-2 sm:p-4 sm:flex">
-                                        <h2 className="card-title text-center flex-initial w-full justify-center">Bu Bölümün Yıldızları 💫</h2>
-                                        {Array.from({ length: 3 }).map((x, index) => (
-                                            <div className='flex flex-row items-center gap-4 justify-start'>
-                                                <h3 className='font-bold'>
-                                                    {index + 1}.
-                                                </h3>
-                                                <div className='flex items-center gap-4'>
-                                                    <div className="avatar">
-                                                        <div className="w-20 rounded-full">
-                                                            <img src="https://placeimg.com/192/192/people" />
+                                <div className='md:sticky md:top-3'>
+                                    <div className="card card-bordered w-full bg-base-100  shadow-2xl">
+                                        <div className="card-body space-y-3 sm:space-y-0 p-2 sm:p-4 sm:flex">
+                                            <h2 className="card-title text-center flex-initial w-full justify-center">KodChallenge Yıldızları 💫</h2>
+                                            {Array.from({ length: 3 }).map((x, index) => (
+                                                <div className='flex flex-row items-center gap-4 justify-start'>
+                                                    <h3 className='font-bold'>
+                                                        {index + 1}.
+                                                    </h3>
+                                                    <div className='flex items-center gap-4'>
+                                                        <div className="avatar">
+                                                            <div className="w-20 rounded-full">
+                                                                <img src="https://placeimg.com/192/192/people" />
+                                                            </div>
+                                                        </div>
+                                                        <div>
+                                                            <h4 className='font-bold'>RaSGooL</h4>
+                                                            <p className='badge badge-accent'>Skor: <strong>1075</strong></p>
                                                         </div>
                                                     </div>
-                                                    <div>
-                                                        <h4>RaSGooL</h4>
-                                                        <p className='badge badge-accent'>Skor: <strong>1075</strong></p>
-                                                    </div>
                                                 </div>
-                                            </div>
-                                        ))}
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
