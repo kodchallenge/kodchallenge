27781395797ce49e881616e2e929943e3fdff63d
Yasin Torun <yasintorun4680@gmail.com>
2023-02-04 14:45:55 +0300

    Added _document.tsx and update color style in footer

---
+++ apps/client/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="tr" data-theme="dracula">
+            <Head>
+
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
