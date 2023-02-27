6ac157d9d846c588399483f0c4d360e6aa28509d
Yasin Torun <yasintorun4680@gmail.com>
2023-02-27 21:13:37 +0300

    feat: added google analtys and favicon

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -1,14 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
     return (
-        <Html lang="tr" data-theme="light">
+        <Html lang="tr" data-theme="dracula">
             <Head>
-
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
             </Head>
             <body>
                 <Main />
                 <NextScript />
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-J3PXNERGEF"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'g-J3PXNERGEF');
+        `}
+                </Script>
             </body>
         </Html>
     )
