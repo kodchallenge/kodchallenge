f222f91f43b75fbff1962099f461d01e8ebbcb8c
Yasin Torun <yasintorun4680@gmail.com>
2023-04-02 12:07:31 +0300

    Added tab tool tip

--- apps/client/components/editor/InfoTabs.tsx
+++ apps/client/components/editor/InfoTabs.tsx
@@ -7,9 +7,9 @@ import { useRouter } from 'next/router'
 const InfoTabs = () => {
     const router = useRouter()
     const [tabs] = useState([
-        { name: 'Açıklama', tab: 'description' },
-        { name: 'Çözümler', tab: 'solutions' },
-        { name: 'Kaydettiklerin', tab: 'submissions' },
+        { name: 'Açıklama', tab: 'description', tooltip: "Problem detayı" },
+        { name: 'Çözümler', tab: 'solutions', tooltip: "Kullanıcı çözümleri" },
+        { name: 'Kaydettiklerin', tab: 'submissions', tooltip: "Kaydettiğin tüm çözümler" },
     ])
 
     const handleSelectTab = (index: number) => {
@@ -21,8 +21,10 @@ const InfoTabs = () => {
         <Tabs onSelect={handleSelectTab} selectedIndex={Math.max(0, tabs.findIndex(x => x.tab == router.query?.tab))} selectedTabClassName='tab-active' className={"h-full relative flex flex-col flex-1"}>
             <TabList className={"tabs bg-base-200 space-x-5"}>
                 {tabs.map((tab, index) => (
-                    <Tab key={index} className={"tab tab-lifted"}>
-                        {tab.name}
+                    <Tab key={index} className={"tab tab-lifted tooltip tooltip-bottom"}>
+                        <div className='tooltip tooltip-bottom' data-tip={tab.tooltip}>
+                            {tab.name}
+                        </div>
                     </Tab>
                 ))}
             </TabList>
