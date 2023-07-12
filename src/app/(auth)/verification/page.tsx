e8eefd39d2409c6a1ba6b73234f6e204eea65974
Yasin Torun <yasintorun4680@gmail.com>
2023-07-12 21:22:01 +0300

    Create verification page

---
+++ src/app/(auth)/verification/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import AuthCard from '../components/AuthCard'
+
+const page = () => {
+    return (
+        <AuthCard
+            description='Doğrulama Kodu'
+            info='Lütfen e-posta adresinize (ornek@kodchallenge) gönderilen doğrulama kodunu giriniz.'
+        >
+            <form>
+                <div className="grid w-full items-center gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                        <label htmlFor="code">Doğrulama Kodu</label>
+                        <Input id="code" placeholder="Doğrulama kodunu giriniz" />
+                    </div>
+                </div>
+                <div className='mt-5'>
+                    <Button className='w-full'>
+                        Doğrula
+                    </Button>
+                </div>
+            </form>
+        </AuthCard>
+    )
+}
+
+export default page
