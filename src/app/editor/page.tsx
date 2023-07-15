23dbbf75c5ac55467ae4dfcaf5e8c80a8699b6b0
Yasin Torun <yasintorun4680@gmail.com>
2023-07-15 21:21:57 +0300

    Done split with tabs

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,4 +1,4 @@
-"use client" 
+"use client"
 import { Logo } from '@/components/logo'
 import { Markdown } from '@/components/markdown'
 import { Button } from '@/components/ui/button'
@@ -7,8 +7,9 @@ import "./editor.css"
 import ProblemListButton from '@/app/editor/components/ProblemListButton'
 import MonacoEditor from "@monaco-editor/react";
 import editor from 'monaco-editor'
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { cn } from '@/lib/utils'
+import Split from 'react-split'
 const md = `## Test
 
 
@@ -65,8 +66,12 @@ const a = 1
 `
 
 const page = () => {
-
   const editorRef = React.useRef<editor.editor.IStandaloneCodeEditor>()
+
+  // useEffect(() => {
+  //   editorRef
+  //   setShowConsole(false)
+  // }, [])
 
   return (
     <div className='h-full flex flex-col max-h-screen overflow-hidden'>
@@ -90,8 +95,13 @@ const page = () => {
         </div>
       </header>
       <main className='h-full flex-1 bg-slate-100'>
-        <div className='px-4 h-full py-2 flex gap-2'>
-          <section className='h-full flex-col flex flex-1 shadow e-side-info border bg-white'>
+        <Split
+          className='px-4 h-full py-2 flex'
+          gutterSize={8}
+          minSize={0}
+          snapOffset={50}
+        >
+          <section className='h-full flex-col flex flex-11 e-side-info border bg-white'>
             <div className='h-10 border-b'></div>
             <div className='h-0 flex-auto overflow-auto p-2'>
               <Markdown markdown={md} />
@@ -109,26 +119,48 @@ const page = () => {
               </div>
             </Tabs> */}
           </section>
-          <section className='h-full flex-1 flex flex-col shadow border bg-white e-code-editor'>
-            <div className='h-10 border-b'>
+          <div>
+            <Split
+              className='h-full flex-1 flex flex-col justify-between e-code-editor'
+              gutterSize={8}
+              direction='vertical'
+              sizes={[70, 30]}
+            >
+              <section className='flex flex-col border bg-white !w-full'>
+                <div className='h-10 border-b'>
 
-            </div>
-            <div className='h-0 flex-auto overflow-auto'>
-              <MonacoEditor
-                // height={height}
-                language={"javascript"}
-                value={"const a = 1"}
-                theme={"light"}
-                beforeMount={(monaco) => {
-                  // monaco.editor.defineTheme("dark", Dark)
-                }}
-                onMount={(editor, monaco) => {
-                  editorRef.current = editor
-                }}
-              />
-            </div>
-          </section>
-        </div>
+                </div>
+                <div className='h-0 flex-1 flex-grow overflow-auto !w-full'>
+                  <MonacoEditor
+                    // height={height}
+                    language={"javascript"}
+                    value={"const a = 1"}
+                    theme={"light"}
+                    beforeMount={(monaco) => {
+                      // monaco.editor.defineTheme("dark", Dark)
+                    }}
+                    onMount={(editor, monaco) => {
+                      editorRef.current = editor
+                    }}
+                  />
+                </div>
+              </section>
+              <section className={'flex flex-col bg-white border min-h-0'}>
+                  {/* <div className='h-10 border-y'>
+
+                  </div> */}
+                  <div className='h-0 flex-auto overflow-auto'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tempore ut officiis impedit neque, iusto modi aliquid, a magni labore expedita libero velit dicta pariatur aliquam sit, voluptate obcaecati. Recusandae nulla magnam quibusdam culpa, dignissimos nihil aperiam consequatur rerum nemo doloremque ex eius voluptas suscipit vitae labore dolorem voluptatibus odit omnis! Facilis quos fugit laborum omnis aperiam, pariatur perferendis, placeat eligendi odit ex assumenda quidem incidunt consequatur itaque at ipsum esse amet enim sequi? Aspernatur molestias provident, commodi, repudiandae eius corporis veniam ipsam omnis iure ab quisquam suscipit culpa necessitatibus laboriosam praesentium expedita est fugit cupiditate perferendis! Quidem autem aut, asperiores temporibus aliquam earum eaque totam, iste, odit labore qui reiciendis pariatur? Totam harum qui architecto neque animi, fugiat dicta! Magni voluptate consectetur officia harum asperiores ab autem iusto praesentium enim sapiente porro eligendi nam, illum blanditiis sit ipsum temporibus explicabo dolorum similique doloremque natus, perferendis id. Minima itaque enim non ab debitis vitae laudantium facere minus, eum, placeat provident nesciunt? A quis nisi fugit rerum suscipit, veritatis enim iusto perspiciatis possimus dolore! Dolorum delectus tenetur omnis quibusdam eos sint animi culpa alias necessitatibus at? Necessitatibus, eius atque! Eum tenetur dolore deleniti officia nostrum? Earum quas fugiat voluptatum ipsum veritatis quidem beatae nulla architecto ducimus repudiandae ipsam commodi facere culpa ad voluptate, natus nemo. Maiores eum error aperiam commodi mollitia fuga porro velit doloremque nam culpa qui reprehenderit ut repudiandae possimus molestiae distinctio consectetur, necessitatibus illo quas odio at! Sunt, maxime. Molestiae quam possimus iure ipsam accusamus facere delectus eveniet laboriosam, expedita amet quasi ratione incidunt fugiat labore recusandae earum impedit esse ea at, ex quidem non, corrupti consequatur! Natus optio nulla tenetur quisquam at est reprehenderit minima. Quos suscipit dolores ipsum tempore rem inventore ipsam vitae sit, molestiae totam libero modi quia aperiam quod nisi consectetur nam accusamus dolorum cupiditate maiores ducimus facere excepturi sapiente! Totam aut corrupti sequi facilis voluptate ducimus tempore perspiciatis, explicabo iusto consectetur quos alias exercitationem culpa assumenda deserunt error debitis sint praesentium! Nemo est tempore enim, odit suscipit ut aut atque itaque dolorem eius praesentium esse? Quasi ex optio sequi aspernatur. Pariatur aut doloribus ab animi dicta dignissimos harum odio autem illo velit iusto mollitia vero distinctio ipsa quia accusantium, voluptas vitae libero. Officia omnis dolorem aliquid itaque exercitationem tempore, veniam, pariatur numquam consequuntur nulla provident voluptates quaerat? Quo doloribus odio accusantium laborum vitae consequuntur numquam optio. Facilis, fugiat quidem ad saepe mollitia dolorum quos corrupti! Fugiat atque distinctio, quasi cupiditate laboriosam aliquam officiis tempora, corporis ex quisquam, quos itaque mollitia. Atque labore cumque, debitis quidem inventore, earum modi rem commodi, a ad aliquid eius tempora illum totam eaque quas ipsam voluptatibus omnis incidunt quo odit. Provident, inventore quis! Ipsum dignissimos tempora atque minima. Quae, eligendi. Necessitatibus debitis rem facilis reiciendis voluptates! Aperiam nulla reiciendis nesciunt eum modi eveniet delectus molestias vel voluptas aspernatur tempore, dolore illum sapiente asperiores inventore perferendis tempora. Officiis eum maiores facilis voluptate ipsa totam illo odio, alias deleniti id harum magni molestiae error itaque! Harum aperiam quam repellendus nihil officiis reprehenderit et architecto id tempore rem ipsam blanditiis sapiente, quidem eius unde quia vitae provident. Inventore officia autem facere vitae, rerum quam expedita similique a natus aliquid, assumenda at obcaecati, nobis nemo illo error harum consequuntur iste perferendis ea reprehenderit. Omnis illo velit numquam aspernatur, nam praesentium molestiae distinctio optio eius ducimus explicabo sunt ab impedit, quidem quisquam vero amet? Ipsa, ipsam exercitationem officia voluptates minus ratione placeat, commodi aliquid quis, voluptatem nulla illo provident quaerat molestias! Consectetur facere dolore explicabo voluptate possimus sed voluptatem, officiis fuga error, officia soluta deserunt voluptas, sit repellat optio labore adipisci ex facilis. Esse nesciunt quo id harum cumque explicabo, ducimus provident assumenda a et! Dolor, corporis temporibus exercitationem voluptatem deserunt aspernatur aliquam vel quisquam nesciunt sequi modi pariatur quas animi accusamus obcaecati dolores quis ut quae amet in? Inventore atque voluptas non eius labore esse illum nesciunt nisi sed fuga numquam repudiandae, fugit ullam impedit quos sunt rem quaerat nemo fugiat ea pariatur doloremque adipisci. Omnis doloribus nesciunt voluptates error culpa recusandae quas modi! Tempora id ea perferendis earum! Nostrum ea molestias, debitis at tempore dolore aspernatur perspiciatis magni asperiores dolores eos explicabo obcaecati doloremque ad? Hic veritatis voluptas deleniti voluptate vitae reprehenderit ducimus, at aperiam molestiae excepturi, quia reiciendis omnis temporibus itaque incidunt sunt ex enim dicta corrupti neque tempore nihil minus voluptatem unde. Voluptates mollitia, dolor nam tenetur libero eum quo repellat a natus incidunt quisquam, id dicta ab at veritatis ex nihil officia unde fugit quae laboriosam totam. Molestiae laboriosam quibusdam illo nemo, nostrum quaerat, itaque aliquam totam cupiditate iusto sit explicabo, corporis quos? Blanditiis, ea placeat officia nam totam voluptatem nemo unde autem maiores eligendi corrupti ducimus animi culpa a, quasi, fugit ab ipsa. Assumenda nihil commodi deleniti deserunt labore nemo nesciunt consequuntur, illo ipsum? Quidem nihil voluptas debitis doloribus adipisci accusamus quae optio beatae sequi veritatis unde quis velit, numquam delectus dicta sit alias autem saepe quo officiis, consectetur pariatur? Dicta saepe fugiat quis nostrum, accusantium aliquid ipsa tempora molestias reprehenderit culpa, molestiae, pariatur in quia quod nihil quos delectus velit. Illum quaerat veritatis quisquam, eaque quo animi dolor? Ullam distinctio ipsum modi omnis ad error harum minus ducimus soluta repellendus obcaecati fuga neque voluptatibus officiis consequatur eos accusantium repellat, libero dolor natus perspiciatis ex! Totam cum officia quia vero voluptates, odit doloremque quibusdam ratione, laudantium accusamus voluptate nesciunt libero perspiciatis rerum accusantium molestias laboriosam facere qui iste non quis! Voluptates tenetur laudantium, consectetur laborum non voluptas molestias atque consequuntur necessitatibus adipisci expedita officiis in sed maxime qui porro? Aperiam eaque voluptatum excepturi et pariatur ipsa a, aliquid asperiores voluptatibus deserunt nostrum officiis aliquam maiores dolor omnis magnam quas officia! Iusto suscipit saepe molestiae quae. Eaque fuga a nemo eveniet sequi molestiae itaque expedita tenetur deserunt vero nam incidunt quo accusantium temporibus ab natus perspiciatis blanditiis ut, et asperiores corrupti! Dolorem nihil odio placeat blanditiis fugiat, velit adipisci sed unde voluptates veritatis voluptatibus, maiores at enim eos provident porro cupiditate quasi hic modi accusantium numquam. Excepturi, dolore!
+                  </div>
+                <section>
+                  <div className='h-10'>
+                    <Button size={"sm"}>Konsol</Button>
+                  </div>
+                </section>
+              </section>
+            </Split>
+          </div>
+        </Split>
       </main>
     </div>
   )
