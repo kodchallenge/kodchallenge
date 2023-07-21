71bdbc78d3000d4c079852c698f7c5f4dfc1fbe3
Yasin Torun <yasintorun4680@gmail.com>
2023-07-21 21:45:38 +0300

    Created solutions design

--- src/app/(modules)/problems/[slug]/(editor)/page.tsx
+++ src/app/(modules)/problems/[slug]/(editor)/page.tsx
@@ -17,12 +17,14 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { THEMES } from '@/constants'
 import { cn } from '@/lib/utils'
 import MonacoEditor from "@monaco-editor/react"
-import { CaretSortIcon, CheckIcon, CopyIcon, EnterFullScreenIcon, GearIcon, MoonIcon, UpdateIcon } from '@radix-ui/react-icons'
+import { CaretSortIcon, ChatBubbleIcon, CheckIcon, CopyIcon, EnterFullScreenIcon, EyeOpenIcon, GearIcon, MoonIcon, ThickArrowUpIcon, UpdateIcon, UploadIcon } from '@radix-ui/react-icons'
 import { useTheme } from "next-themes"
 import React from 'react'
 import Split from 'react-split'
 import "./editor.css"
 import Link from 'next/link'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
 
 const md = `## Test
 
@@ -249,7 +251,48 @@ const page = () => {
                 <TabsContent value="description">
                   <Markdown markdown={md} />
                 </TabsContent>
-                <TabsContent value="solutions">Change your password here.</TabsContent>
+                <TabsContent value="solutions">
+                  <div>
+                    {Array.from({ length: 10 }).map((_, i) => (
+                      <div key={i} className='px-2 py-4 hover:bg-slate-50 dark:hover:bg-slate-700 cursor-pointer rounded-sm'>
+                        <h1 className='text-lg font-medium mb-2'>✅3 Method's || C++ || JAVA || PYTHON || Beginner Friendly🔥🔥🔥</h1>
+                        <div className='flex flex-wrap space-y-1 items-center space-x-3'>
+                          <Link href={"#"} className='flex items-center space-x-1'>
+                            <Avatar className='h-5 w-5'>
+                              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                              <AvatarFallback>CN</AvatarFallback>,
+                            </Avatar>
+                            <h2 className='text-sm font-medium'>shadcn</h2>
+                          </Link>
+                          <div className='text-xs text-muted-foreground'>
+                            09 Temmuz 2023
+                          </div>
+                          <div className='flex space-x-1 flex-1'>
+                            {["C++", "Java", "Python"].map((label, i) => (
+                              <Badge variant={"secondary"} key={i}>{label}</Badge>
+                            ))}
+                          </div>
+                        </div>
+                        <div>
+                          <div className='flex space-x-2 items-center'>
+                            <Button className='space-x-2' variant={"ghost"} disabled>
+                              <ThickArrowUpIcon />
+                              <span className='text-xs'>682</span>
+                            </Button>
+                            <Button className='space-x-2' variant={"ghost"} disabled>
+                              <EyeOpenIcon />
+                              <span className='text-xs'>1.5K</span>
+                            </Button>
+                            <Button className='space-x-2' variant={"ghost"} disabled>
+                              <ChatBubbleIcon />
+                              <span className='text-xs'>3</span>
+                            </Button>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </TabsContent>
                 <TabsContent value="submissions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eligendi assumenda atque aliquam iure illo ducimus voluptas nulla enim natus possimus iusto aliquid vel quo odio reiciendis, error nam suscipit officia, quidem culpa? Corporis incidunt et, perspiciatis quas amet eaque laudantium cupiditate voluptatem minus repellat accusamus id, a impedit aliquid. Minima sit quam, quia quae, laboriosam sint nesciunt quo quis, labore provident reprehenderit ipsam reiciendis aperiam eos cum. Vero, maxime error aliquid iusto explicabo velit, beatae pariatur eligendi esse similique delectus iste quae aperiam dolorem! Laudantium, eius natus, odit nulla maiores doloremque facilis ipsa doloribus, quasi eos accusantium quae labore deserunt expedita adipisci! Sint tempore iusto obcaecati impedit corrupti quidem non commodi suscipit maxime velit excepturi, repudiandae recusandae dignissimos sequi, voluptatum, aut vitae inventore quisquam animi ex! Modi facilis nemo quibusdam at dolorem accusantium neque quod corrupti quas. Tempore ratione sint rerum nemo eum earum porro, provident inventore, debitis quis, sequi optio totam deserunt. Nam ipsa, possimus aut sapiente ullam et sit quaerat. Iusto cumque deleniti sint tempore eligendi dicta provident molestias nihil natus eos, fugiat nisi illo, numquam vitae a incidunt explicabo fuga esse voluptates iste corrupti quis amet consequuntur. Numquam provident reprehenderit veritatis ab, praesentium laborum impedit non ipsam fugit animi. Eos itaque placeat atque, corporis quam recusandae ullam quod, doloribus ducimus exercitationem delectus beatae, enim pariatur! Voluptatum, rem eum possimus itaque expedita, reprehenderit aliquam beatae distinctio nobis praesentium maiores voluptate ipsum! Nisi, cumque quis. Adipisci odit, rem necessitatibus maiores praesentium architecto quis quae soluta inventore ex quia sint quas laborum sequi natus autem sit ullam! Aperiam ducimus deserunt dolorem eos ipsa porro rem, at voluptas consectetur mollitia facere iure, fugit temporibus accusantium totam dolore fuga quasi delectus cumque eius, facilis alias laboriosam aut magni. Quisquam, maxime tenetur ipsum distinctio sequi reiciendis culpa adipisci nihil deleniti et neque quis quae, similique nulla nesciunt voluptatem soluta delectus explicabo molestias molestiae! Ipsam odio nisi eos, facere exercitationem aliquam quas unde officiis! Cupiditate temporibus quasi numquam fugiat quae recusandae odit. Ad doloribus commodi eveniet eligendi labore fugit quia deserunt magni porro amet distinctio repudiandae, minus, magnam asperiores. Quidem, eaque rem laudantium libero pariatur neque iure odio quo eos. Minima blanditiis cupiditate magnam sit saepe, sequi fugiat! Minima exercitationem totam iusto. Ad inventore maiores aut vel deleniti harum eaque omnis laborum ullam repellendus nesciunt eligendi incidunt vero possimus atque sunt aliquid, delectus, culpa sequi aperiam fuga molestiae autem repudiandae! Fuga, doloremque delectus ut sunt eveniet praesentium tenetur quis pariatur? Officia ad sed quos maiores ipsam nemo quae molestias minus expedita iure nisi totam quia voluptate quis magnam ex, ut eum voluptatem fugit est assumenda delectus sint quaerat ipsum? A quia, placeat tempore sapiente reiciendis, enim perferendis, eius doloribus sit dolore pariatur magni. Debitis vitae molestiae qui nisi, earum sit quod nostrum accusantium voluptatem exercitationem cupiditate maxime saepe iste? Eaque odit deserunt nobis! Dolore molestiae illo deserunt ad at consectetur harum eos beatae ea incidunt temporibus dicta fugit delectus autem porro, tempore eum, ullam et est aut repellendus quod eaque. Natus inventore atque excepturi similique voluptas quas modi praesentium tenetur, maiores numquam laboriosam unde, tempora, facere corrupti! Dolorem animi placeat deleniti aperiam illum officia, magni minus harum! Labore esse eligendi veniam molestias odio totam neque voluptatem cupiditate eum. Deleniti aperiam dolore libero iste minima consequatur explicabo nobis voluptatem facere quas harum ratione inventore expedita delectus accusamus sed repellendus, eum veniam in fuga perferendis esse aliquam? Asperiores quae consequuntur aliquam quod possimus dolore a corrupti, rem earum modi voluptas doloribus explicabo nihil nobis! Eveniet architecto repellendus ea numquam dolor fugit voluptate quasi esse odio et at porro est asperiores culpa, molestias sequi quod ratione repellat deleniti quibusdam aspernatur nemo nihil quam. Reprehenderit iusto quia, veritatis molestias voluptates porro dicta quae necessitatibus autem! Amet impedit quasi laboriosam dolorem aperiam sint, quam consequatur laudantium et. Quas quasi non architecto tempora consequatur dolores, atque adipisci veniam. Animi repellendus impedit vitae cumque asperiores dicta id maxime delectus, consequuntur totam eaque deleniti ab velit! Totam ut iusto quae ducimus error a exercitationem maxime, voluptates magnam corrupti est reiciendis expedita amet corporis voluptatem adipisci animi laborum asperiores! Dolorem quidem quibusdam ex dolore velit dolores possimus cumque, facilis vitae nesciunt placeat nemo? Ab voluptas blanditiis eum natus. Vitae atque cumque repellendus nam voluptatum dolor totam! Molestiae, nostrum libero delectus sit cumque dolore beatae qui earum doloremque expedita pariatur sint explicabo repellat eaque, quas error reiciendis vero dolores. Repellat unde nostrum atque cum dolore commodi cumque iusto, ex ipsam illum dolor sequi blanditiis. Nulla eveniet neque doloremque aut laborum qui a ipsam nihil impedit accusamus non nemo maxime deserunt modi iusto ad dolore aliquam vitae necessitatibus perspiciatis voluptatem dignissimos, et quis! Dolore unde pariatur aspernatur dignissimos, cupiditate optio exercitationem, a architecto labore facilis perferendis, saepe rerum at asperiores corporis necessitatibus sit voluptatibus ratione quos beatae repellat consequuntur deleniti officiis? Aliquam, sunt nemo vel ad sed libero quo velit expedita blanditiis illum! Adipisci expedita ipsum, laboriosam beatae accusamus fuga iusto. Laudantium vitae aliquid qui accusamus ducimus omnis quia asperiores, numquam necessitatibus illo aliquam nesciunt impedit ad maxime tempore officiis ut molestias odit laboriosam quos perspiciatis soluta? Placeat sit voluptates voluptatibus laboriosam, illum fugit accusamus optio expedita quam dolorem neque sint libero est, velit tenetur ratione et. Unde molestiae natus ipsa soluta et, ut sapiente animi fugiat. Quisquam quam illum deserunt voluptas commodi expedita delectus, corrupti nesciunt accusamus quae excepturi eos quos. Quis soluta voluptas temporibus fugit tenetur ipsum! Tempore impedit quidem alias quisquam culpa, temporibus quia modi neque architecto nemo harum molestias. Est quo odit laboriosam laudantium placeat ipsum praesentium inventore sapiente necessitatibus minus quisquam, excepturi omnis tenetur labore reiciendis adipisci veritatis similique, voluptas nihil molestias impedit esse mollitia consequuntur iusto! Sint, quod debitis odit, magnam in rerum, quaerat nemo earum voluptatem ullam obcaecati repellat voluptas quis praesentium. Esse vitae debitis libero quidem quaerat eius voluptatem ratione sed amet dignissimos consectetur animi tempore nulla temporibus nesciunt ex modi similique, in cupiditate nostrum ab omnis repudiandae harum? Inventore eligendi in iusto quae ullam sequi earum ipsa dolore neque? Nam quaerat corporis quibusdam ullam amet!</TabsContent>
               </div>
             </div>
