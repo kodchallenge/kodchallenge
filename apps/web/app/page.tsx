90554794baad7167a93a2491b04b6d989aa70bd7
Yasin Torun <yasintorun4680@gmail.com>
2023-09-09 10:14:28 +0300

    created monorepo with turbo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@kod/ui";
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="grid place-items-center h-screen w-full">
+      <Button>Test</Button>
+    </main>
+  );
+}
