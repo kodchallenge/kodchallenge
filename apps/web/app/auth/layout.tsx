288abe749bd3a28defe9ac9a70bb11291ee8e320
Yasin Torun <yasintorun4680@gmail.com>
2023-10-05 19:32:55 +0300

    chore: Changed background image for auth layout

--- apps/web/app/auth/layout.tsx
+++ apps/web/app/auth/layout.tsx
@@ -5,9 +5,12 @@ export default function AuthLayout({
 }: React.PropsWithChildren<{}>) {
 
   return (
-    <main className='min-h-screen container flex flex-col items-center justify-center bg-[url("/images/auth-bg.png")]'>
-      <div className='flex flex-col items-center justify-center space-y-4 py-2'>
-        {children}
+    <main className='min-h-screen flex flex-col items-center justify-center'>
+      <div className='-z-10 absolute top-0 left-0 w-full h-full bg-[url("/assets/images/home-bg.png")] bg-center bg-no-repeat bg-cover opacity-20 '></div>
+      <div className="container">
+        <div className='flex flex-col items-center justify-center space-y-4 py-2'>
+          {children}
+        </div>
       </div>
     </main>
   )
